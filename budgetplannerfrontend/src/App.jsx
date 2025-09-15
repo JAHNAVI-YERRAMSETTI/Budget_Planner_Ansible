af16b2e3df560406674523d80e32feb097c8528f
Adithya726 <adithya4200@gmail.com>
2025-09-15 18:45:26 +0530

    added frontend

---
+++ budgetplannerfrontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminDashboard from './admin/AdminDashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Budget Planner</h1>
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default App;
